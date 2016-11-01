63a4fed515204f98947720ca1419df62e7e094ec
rsstdd <rosstod@gmail.com>
2016-10-31 19:59:13 -0700

    Add dependencies; Wrote server

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  extends: [
+    'ryansobol/browser',
+    'ryansobol/es5',
+    'ryansobol/es6',
+    'ryansobol/jquery',
+    'ryansobol/node',
+    'ryansobol/materialize',
+    'ryansobol/express'
+  ]
+};
