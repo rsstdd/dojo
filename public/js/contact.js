a3c91e868a7df3b5a65f98c0e90635f8295b8f24
rsstdd <rosstod@gmail.com>
2016-11-01 08:56:57 -0700

    Partially completed contact form

---
+++ public/js/contact.js
@@ -0,0 +1,22 @@
+'use strict'
+
+(function(){
+  const first = $('#first')
+  const last = $('#last')
+  const last = $('#last')
+  const phone = $('#phone')
+  const student = $('#student')
+  const formEmail = $('#formEmail');
+  const subject = $('#subject');
+  const text = $('#text');
+  const mailOptions = {};
+
+  const mailOptions = {
+    from: 'example@gmail.com>', // sender address
+    to: 'receiver@destination.com', // list of receivers
+    subject: 'Email Example', // Subject line
+    text: text
+    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+  };
+
+});
