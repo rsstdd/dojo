f48cd439c920e4ad7ee94085b6db38ac70e6a1d5
rsstdd <rosstod@gmail.com>
2016-12-11 11:13:04 -0800

    Updated README text

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,22 +1,45 @@
-'use strict'
+'use strict';
+(function() {
+  $('#contactForm').submit((event) => {
+    event.preventDefault();
 
-$(function(){
-  const first = $('#first')
-  const last = $('#last')
-  const last = $('#last')
-  const phone = $('#phone')
-  const student = $('#student')
-  const formEmail = $('#formEmail');
-  const subject = $('#subject');
-  const text = $('#text');
-  const mailOptions = {};
+    console.log('Hello');
 
-  const mailOptions = {
-    from: 'example@gmail.com>', // sender address
-    to: 'receiver@destination.com', // list of receivers
-    subject: 'Email Example', // Subject line
-    text: text
+    const email = $('#email').val();
+    const first = $('#firstName').val().trim();
+    const last = $('#last-name').val().trim();
+    const student = $('#student').val().trim();
+    const fname = $('#firstName').val().trim();
+    const subject = $('#subject').val().trim();
+    const text = $('#text').val().trim();
+
+    const mailOptions = {};
+// , last, phone, student, formEmail, subject, text
+    console.log(fname, last, email, student, email, subject, text);
+
+    const options = {
+      contentType: 'application/json',
+      data: JSON.stringify({ fname, last, email, student, subject, text }),
+      dataType: 'json',
+      type: 'POST',
+      url: '/contact'
+    };
+
+    $.ajax(options)
+      .done(() => {
+        window.location.href = '/';
+      })
+      .fail(($xhr) => {
+        Materialize.toast($xhr.responseText, 3000);
+      });
+
+    // const mailOptions = {
+    //   from: formEmail, // sender address
+    //   to: 'rosstod@gmail.com', // list of receivers
+    //   subject: 'Someone is Interested', // Subject line
+    //   text: text
     // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
-  };
+    // }
 
-});
+  });
+})();
--- routes/contact.js
+++ routes/contact.js
@@ -6,7 +6,11 @@ const nodemailer = require('nodemailer');
 const ev = require('express-validation');
 const validations = require('../validations/airplanes');
 
-router.post('/', (req, res, _next) => {
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+
+
+router.post('/contact', (req, res, _next) => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
@@ -16,6 +20,26 @@ router.post('/', (req, res, _next) => {
   });
 })
 
+
 const router = express.Router();
 
 module.exports = router;
+
+// create reusable transporter object using the default SMTP transport
+
+// // setup e-mail data with unicode symbols
+// var mailOptions = {
+//     from: '"Fred Foo 👥" <foo@blurdybloop.com>', // sender address
+//     to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+//     subject: 'Hello ✔', // Subject line
+//     text: 'Hello world 🐴', // plaintext body
+//     html: '<b>Hello world 🐴</b>' // html body
+// };
+//
+// // send mail with defined transport object
+// transporter.sendMail(mailOptions, function(error, info){
+//     if(error){
+//         return console.log(error);
+//     }
+//     console.log('Message sent: ' + info.response);
+// });
